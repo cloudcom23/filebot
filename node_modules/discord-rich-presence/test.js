cda8db980f843714af6615af8f296d2ef0d5f3e6
cloudcom23 <log866603@gmail.com>
2019-06-20 08:50:19 +0800

    node_modules

---
+++ node_modules/discord-rich-presence/test.js
@@ -0,0 +1,39 @@
+'use strict';
+
+const client = require('.')('180984871685062656');
+
+client.on('join', (secret) => {
+  console.log('we should join with', secret);
+});
+
+client.on('spectate', (secret) => {
+  console.log('we should spectate with', secret);
+});
+
+client.on('joinRequest', (user) => {
+  if (user.discriminator === '1337') {
+    client.reply(user, 'YES');
+  } else {
+    client.reply(user, 'IGNORE');
+  }
+});
+
+client.on('connected', () => {
+  console.log('connected!');
+
+  client.updatePresence({
+    state: 'slithering',
+    details: '🐍',
+    startTimestamp: new Date(),
+    largeImageKey: 'snek_large',
+    smallImageKey: 'snek_small',
+    partyId: 'snek_party',
+    partySize: 1,
+    partyMax: 1,
+    matchSecret: 'slithers',
+    joinSecret: 'boop',
+    spectateSecret: 'sniff',
+  });
+});
+
+process.on('unhandledRejection', console.error);
